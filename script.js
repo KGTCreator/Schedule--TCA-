dc995dcff2f0f653fe641c3fdd5edfe5a1031ebe
KGTCreator <524Ktobin@frhsd.com>
2024-01-25 08:16:31 -0500

    Code Repairs

--- script.js
+++ script.js
@@ -22,25 +22,20 @@ $(document).ready(function () {
         populateTable(filteredData)
       })
     })
-    // Function to populate the table with schedule data
-    function populateTable(scheduleData) {
-        $('#schedule-table-body').empty();
-
-        $.each(scheduleData, function (i, schedule) {
-            let row = "<tr>";
-            // Add the class name, teacher name, room number, and days to the row
-            row += '<td>' + schedule.class_name + '</td>'
-            row += '<td>' + schedule.teacher_name + '</td>'
-            row += '<td>' + schedule.room_number + '</td>'
-            row += '<td>' + schedule.cdays.join(', ') + '</td>'
-            row += '<tr>'
-        })
-
-        let row = "<tr>";
-        row += "<td>" + schedlue.class_name + "<td>";
-
-        $('#schedule-table-body').append(row)
-
-    }
-
+      // Function to populate the table with schedule data
+  function populateTable (scheduleData) {
+    $('#schedule-table-body').empty()
+    // Iterate over each object in the scheduleData array
+    $.each(scheduleData, function (i, schedule) {
+      let row = '<tr>'
+      // Add the class name, teacher name, room number, and days to the row
+      row += '<td>' + schedule.class_name + '</td>'
+      row += '<td>' + schedule.teacher_name + '</td>'
+      row += '<td>' + schedule.room_number + '</td>'
+      row += '<td>' + schedule.days.join(', ') + '</td>'
+      row += '</tr>'
+      // Append the new row to the table body
+      $('#schedule-table-body').append(row)
+    })
+  }
 })
